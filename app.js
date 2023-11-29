bdb6a435a7489319d7d6c0200844fa5b2ff99a7e
unknown <brosjayme@gmail.com>
2023-11-29 10:51:51 +0100

    first commit

--- app.js
+++ app.js
@@ -14,11 +14,16 @@ app.get("/", function (req, res) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
-      console.log(weatherDescription);
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write(
+        "<h1>the temperature in london is " + temp + " degrees celcius.</h1>"
+      );
+      res.write("<img src=" + imageURL + ">");
+      res.send();
     });
   });
-
-  res.send("server is up and running");
 });
 
 app.listen(3000, function () {
